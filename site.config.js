084e71afe2f8d993cab4ffd7c98dd8d990727d05
xiyanlive <82606031+xiyanlive@users.noreply.github.com>
2021-04-21 00:18:57 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'bcced14b5a63439aa5db2d2718fb8c12',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '戏言戏语',
+  domain: '这里有关于戏言的一些戏语',
+  author: '戏言',
 
   // open graph metadata (optional)
   description: 'Example site description',
